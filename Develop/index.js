8f7a815b2555e8b4b49b7edd321eddc57d21c8dd
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2021-02-11 15:09:45 -0700

    issues connecting index to generateMArkdown

--- Develop/index.js
+++ Develop/index.js
@@ -4,61 +4,66 @@ const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [
+    {type: 'input',
+    name: 'email',
+    message: 'Please enter your email address.'},
+
+    {type: 'input',
+    name: 'github',
+    message: 'Please enter your GitHub username.'},
+
    {type: 'input',
     name: 'title',
-    message: 'Enter the title of your project. (Required)'}, 
+    message: 'Enter the title of your project.'}, 
 
     {type: 'input',
     name: 'Description',
-    message: 'Add a description for your project. (Required)'},
-
-    // {type: 'input',
-    // name: 'TableOfContents',
-    // message: 'Create a Table of Contents for your project. (Required)'},
+    message: 'Add a description for your project.'},
 
     {type: 'input',
     name: 'Installation',
-    message: 'What are the steps required to install your project? (Required)'},
+    message: 'What are the steps required to install your project?'},
 
     {type: 'input',
     name: 'Usage',
-    message: 'Provide instructions and examples for use. (Required)'},
+    message: 'Provide instructions and examples for use.'},
     //would you like to add screenshots? T or F, if F then follow up question for image link; else skip
 
     {type: 'checkbox',
     name: 'License',
-    message: 'Select the license used (Required)',
+    message: 'Select the license used',
     choices: ['MIT', 'ISC', 'None of the Above']},
 
     {type: 'input',
     name: 'Contributing',
-    message: 'List your collaborators, if any, with links to their GitHub profiles. (Required)'},
+    message: 'List your collaborators, if any, with links to their GitHub profiles.'},
 
     {type: 'input',
     name: 'Tests',
-    message: 'If you created tests for your application, please provide them below. (Required)'},
+    message: 'If you created tests for your application, please provide them below.'}
 
-    {type: 'input',
-    name: 'questions',
-    message: 'Please enter your email address. (Required)'},
-
-    {type: 'input',
-    name: 'questions2',
-    message: 'Please enter your GitHub username. (Required)'}
 ];
+// Import generateMarkdown
+function importMD(){
+    fs.readFile('./utils/generateMarkdown.js', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+   // console.log(data)
+
+  });
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-// 
-// const generateFile = require('./src/README.md');
+function writeToFile(fileName, data) {
+    importMD();
+ fs.writeFile('/README.md', generateMarkdown(data), err => {
+   if (err) throw err;
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+   console.log('README is complete!');
+ });
+};
 
 // TODO: Create a function to initialize app
 function init() {
@@ -66,7 +71,9 @@ function init() {
         return  inquirer.prompt(questions)
 
          // Write the user response to a file by chaining the below callback method to the prompt above.
-        .then(function(data) {
+        .then(function(data){
+        //writeToFile();
+        // {
             // Bonus: Generate the name of your user file from their input
             // const filename =
             // data.title
@@ -74,16 +81,18 @@ function init() {
             //     .split(' ')
             //     .join('') + '.json';
 
-            fs.writeFile('readme.json', JSON.stringify(data, null, '\t'), function(err) {
-            if (err) {
-                return console.log(err);
-            }
+            // fs.writeFile('readme.json', JSON.stringify(data, null, '\t'), function(err) {
+            // if (err) {
+            //     return console.log(err);
+            // }
 
-            console.log('Your data has been stored');
+            return console.log('Your data has been stored')
+
             });
-        });
-    }
+        };
+    
     promptUser()
+    .then(writeToFile);
 };
 
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,43 +1,60 @@
 //Pull in user answers
 const fs = require('fs')
 
-fs.readFile('/Users/joe/test.txt', 'utf8' , (err, data) => {
+fs.readFile('./readme.json', 'utf8' , (err, data) => {
   if (err) {
     console.error(err)
     return
   }
   console.log(data)
-})
+ })
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-   if (liscSelcet=='MIT'){
-  liscText= 'MIT License'
-+
-'  Copyright (c) [year] [fullname]'
+   if (data.license=='MIT'){
+    badge= '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+
+   } else if (data.license=='ISC'){
+    badge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+  }
+   else if (data.license=='None of the Above'){
+    badge=  '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+ }
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
   
-'  Permission is hereby granted, free of charge, to any person obtaining a copy'
-'  of this software and associated documentation files (the "Software"), to deal'
-'  in the Software without restriction, including without limitation the rights'
-'  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell'
-'  copies of the Software, and to permit persons to whom the Software is'
-'  furnished to do so, subject to the following conditions:'
+}
 
-'  The above copyright notice and this permission notice shall be included in all'
-'  copies or substantial portions of the Software.'
-
-'  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR'
-'  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,'
-'  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE'
-'  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER'
-'  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,'
-'  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE'
-'  SOFTWARE.""'
-
-  badge= '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-
-   } else if (liscSelcet=='ISC'){
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (data.license !=='')
+  if (data.license=='MIT'){
+    liscText= 'MIT License'
+  +
+  '  Copyright (c) [year] [fullname]'
+    
+  '  Permission is hereby granted, free of charge, to any person obtaining a copy'
+  '  of this software and associated documentation files (the "Software"), to deal'
+  '  in the Software without restriction, including without limitation the rights'
+  '  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell'
+  '  copies of the Software, and to permit persons to whom the Software is'
+  '  furnished to do so, subject to the following conditions:'
+  
+  '  The above copyright notice and this permission notice shall be included in all'
+  '  copies or substantial portions of the Software.'
+  
+  '  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR'
+  '  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,'
+  '  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE'
+  '  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER'
+  '  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,'
+  '  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE'
+  '  SOFTWARE.""'
+} else if (data.license=='ISC'){
   liscText= 'ISC License'
 
   'Copyright (c) [year], [fullname]'
@@ -54,26 +71,60 @@ function renderLicenseBadge(license) {
   'ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF'
   'OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.'
 
-  badge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
-  }
-   else if (liscSelcet=='None of the Above'){
+}  else if (data.license=='None of the Above'){
     liscText= 'None'
-  badge=  '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
  }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ##Description
+  ${data.description}
+
+  ##Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits) 
+  * [License](#license) 
+  * [Badges](#badges)
+  * [Features](#features)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ##Installation
+  ${data.installation}
+
+  ##Usage
+  ${data.usage}
+
+  ##Credits
+  ${data.credits}
+
+  ##License
+  renderLicenseSection(license); 
+  renderLicenseSection(license);
+
+  ##Badges
+  renderLicenseBadge(license);
+
+  ##Features
+
+  ##Contributing
+
+  ##Tests
+  
+  ##Questions
+  For any questions please contact me with the information below:
+  Github: [@${data.github}]
+  Email: ${data.email}
+  
+
+`
+
+;
 }
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown(data);
