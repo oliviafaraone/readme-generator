9c4abaeab0437ca1092216641bf3b4073ba76bf0
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2021-02-10 23:48:23 -0700

    inital outline

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,91 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+   {type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project. (Required)'}, 
+
+    {type: 'input',
+    name: 'Description',
+    message: 'Add a description for your project. (Required)'},
+
+    // {type: 'input',
+    // name: 'TableOfContents',
+    // message: 'Create a Table of Contents for your project. (Required)'},
+
+    {type: 'input',
+    name: 'Installation',
+    message: 'What are the steps required to install your project? (Required)'},
+
+    {type: 'input',
+    name: 'Usage',
+    message: 'Provide instructions and examples for use. (Required)'},
+    //would you like to add screenshots? T or F, if F then follow up question for image link; else skip
+
+    {type: 'checkbox',
+    name: 'License',
+    message: 'Select the license used (Required)',
+    choices: ['MIT', 'ISC', 'None of the Above']},
+
+    {type: 'input',
+    name: 'Contributing',
+    message: 'List your collaborators, if any, with links to their GitHub profiles. (Required)'},
+
+    {type: 'input',
+    name: 'Tests',
+    message: 'If you created tests for your application, please provide them below. (Required)'},
+
+    {type: 'input',
+    name: 'questions',
+    message: 'Please enter your email address. (Required)'},
+
+    {type: 'input',
+    name: 'questions2',
+    message: 'Please enter your GitHub username. (Required)'}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+// 
+// const generateFile = require('./src/README.md');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    const promptUser = () => {
+        return  inquirer.prompt(questions)
+
+         // Write the user response to a file by chaining the below callback method to the prompt above.
+        .then(function(data) {
+            // Bonus: Generate the name of your user file from their input
+            // const filename =
+            // data.title
+            //     .toLowerCase()
+            //     .split(' ')
+            //     .join('') + '.json';
+
+            fs.writeFile('readme.json', JSON.stringify(data, null, '\t'), function(err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log('Your data has been stored');
+            });
+        });
+    }
+    promptUser()
+};
+
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,65 @@
+//Pull in user answers
+const fs = require('fs')
+
+fs.readFile('/Users/joe/test.txt', 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(data)
+})
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+   if (liscSelcet=='MIT'){
+  liscText= 'MIT License'
++
+'  Copyright (c) [year] [fullname]'
+  
+'  Permission is hereby granted, free of charge, to any person obtaining a copy'
+'  of this software and associated documentation files (the "Software"), to deal'
+'  in the Software without restriction, including without limitation the rights'
+'  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell'
+'  copies of the Software, and to permit persons to whom the Software is'
+'  furnished to do so, subject to the following conditions:'
+
+'  The above copyright notice and this permission notice shall be included in all'
+'  copies or substantial portions of the Software.'
+
+'  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR'
+'  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,'
+'  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE'
+'  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER'
+'  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,'
+'  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE'
+'  SOFTWARE.""'
+
+  badge= '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+
+   } else if (liscSelcet=='ISC'){
+  liscText= 'ISC License'
+
+  'Copyright (c) [year], [fullname]'
+  
+  'Permission to use, copy, modify, and/or distribute this software for any'
+  'purpose with or without fee is hereby granted, provided that the above'
+  'copyright notice and this permission notice appear in all copies.'
+  
+  'THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES'
+  'WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF'
+  'MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR'
+  'ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES'
+  'WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN'
+  'ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF'
+  'OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.'
+
+  badge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+  }
+   else if (liscSelcet=='None of the Above'){
+    liscText= 'None'
+  badge=  '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+ }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
